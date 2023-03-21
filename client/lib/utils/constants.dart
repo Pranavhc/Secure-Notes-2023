98cf7d38a8efb57383e5761851d896faea58e029
Pranav Chandane <pranavhchandane@gmail.com>
2023-03-21 14:18:05 +0530

    *

--- client/lib/utils/constants.dart
+++ client/lib/utils/constants.dart
@@ -1,2 +1,2 @@
-const server = "https://secure-notes.up.railway.app/api/v1"; // on railway.app
-// const server = "https://secure-notes-api.onrender.com/api/v1"; // on render.com
+// const server = "https://secure-notes.up.railway.app/api/v1"; // on railway.app
+const server = "https://secure-notes-api.onrender.com/api/v1"; // on render.com
